eacb0c71822f0edca23b193deced4f34278e8f83
eduaserr <eduaserr@github.com>
2025-07-30 20:28:55 +0200

    namespaces not allowed

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,49 +6,32 @@
 /*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 16:18:43 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/07/28 19:03:33 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/07/30 20:27:42 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-using namespace std;
-
-namespace getStdin {
-	void	print_param(char **av)
-	{
-		int	i = 0;
-
-		while (av && av[++i])
-			cout << av[i] << ' ';
-		cout << endl;
-	}
-
-	void	ft_toupper(char *av)
-	{	
-		for (int j = 0; av[j]; ++j) {
-			if (av[j] >= 'a' && av[j] <= 'z')
-	 			cout << ((char)(av[j] - 32));
+void	modify_param(char **av)
+{
+	for (int i = 0; av && av[++i];){
+		for (int j = 0; av[i][j]; ++j) {
+			if (av[i][j] >= 'a' && av[i][j] <= 'z')
+ 				std::cout << ((char)(av[i][j] - 32));
 			else
-				cout << av[j];
-		}
-		cout << ' ';
+				std::cout << av[i][j];
 	}
-
-	void	modify_param(char **av)
-	{
-		for (int i = 0; av && av[++i];)
-			ft_toupper(av[i]);
-		cout << endl;
+	std::cout << ' ';
 	}
+	std::cout << std::endl;
 }
 
 int	main(int ac, char **av)
 {
 	//coger parametros
 	if (ac == 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
-		getStdin::modify_param(av);
+		modify_param(av);
 	return (0);
 }
