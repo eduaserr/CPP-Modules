8b3a1102f78b6a44e7455fb0281c7cdd12174683
eduaserr <eduaserr@github.com>
2025-07-30 03:52:22 +0200

    push

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,19 @@
 /*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 22:25:39 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/07/28 22:25:40 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/07/29 22:23:32 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "inc/a.hpp"
+
+Sample::Sample(void){
+	std::cout << "Hola" << std::endl;
+}
+
+int	main(void)
+{
+	Sample obj;
+	std::cout << "hello" << std::endl;
+	return (0);
+}
