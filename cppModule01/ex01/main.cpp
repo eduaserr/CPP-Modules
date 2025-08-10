c1c86261feb95efae65f0ab660b3b94a8da60734
eduaserr <eduaserr@github.com>
2025-08-11 01:21:49 +0200

    optimized code, erased function

--- cppModule01/ex01/main.cpp
+++ cppModule01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 23:48:08 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/08/11 00:55:33 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/08/11 01:19:29 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 int	main(void)
 {
 	Zombie	*newHorde;
+	int		n = 5;
 
-	newHorde = zombieHorde(5, "EDU");
-
+	newHorde = zombieHorde(n, "EDU");
+	for (int i = 0; i < n; i++){
+		newHorde[i].announce();
+	}
 	delete[] newHorde;
 	return (0);
 }
--- cppModule01/ex01/zombieHorde.cpp
+++ cppModule01/ex01/zombieHorde.cpp
@@ -6,24 +6,16 @@
 /*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 04:01:56 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/08/11 00:42:21 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/08/11 01:14:07 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie*	createZombie(int N, std::string name){
+Zombie*	zombieHorde(int N, std::string name){
 	Zombie *horde = new Zombie[N];
 	for (int i = 0; i < N; i++){
 		horde[i].setName(name);
 	}
 	return (horde);
-}
-
-Zombie* zombieHorde(int N, std::string name){
-	Zombie	*Horde = createZombie(N, name);
-	for (int i = 0; i < N; i++){
-		Horde[i].announce();
-	}
-	return Horde;
 }
