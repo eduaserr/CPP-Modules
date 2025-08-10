d0dd9eade82e9e55d3c4b6692cfa32ae5d619929
eduaserr <eduaserr@github.com>
2025-08-11 00:56:52 +0200

    norm

--- cppModule01/ex01/main.cpp
+++ cppModule01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eduaserr <eduaserr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 23:48:08 by eduaserr          #+#    #+#             */
-/*   Updated: 2025/08/11 00:46:04 by eduaserr         ###   ########.fr       */
+/*   Updated: 2025/08/11 00:55:33 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	main(void)
 {
 	Zombie	*newHorde;
 
-	(void)newHorde;
 	newHorde = zombieHorde(5, "EDU");
 
 	delete[] newHorde;
