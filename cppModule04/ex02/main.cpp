02b29fa5000f6c706d4ab2c1fe60d5da3933730f
eduaserr <157403329+eduaserr@users.noreply.github.com>
2025-10-28 19:42:59 +0100

    added

--- cppModule04/ex02/main.cpp
+++ cppModule04/ex02/main.cpp
@@ -3,34 +3,6 @@
 #include "inc/Dog.hpp"
 #include "inc/Cat.hpp"
 
-static Cat*	catIdeas()
-{
-	Cat* cat(new Cat);
-	for (int i = 0; i < 5; i++){
-	std::string arr[] = {"EAT!", "PLAY!", "SLEEP!", "PEE!", "REPEAT!"};
-	cat->getBrain()->setIdea(i, arr[i]);
-	}
-	return (cat);
-}
-
-static Dog*	dogIdeas()
-{
-	Dog* dog(new Dog);
-	for (int i = 0; i < 5; i++){
-	std::string arr[] = {"EAT!", "PLAY!", "SLEEP!", "PEE!", "REPEAT!"};
-	dog->getBrain()->setIdea(i, arr[i]);
-	}
-	return (dog);
-}
-void checkIdeas(Animal* animals[], int size) {
-	for (int i = 0; i < size; i++) {
-		Brain* brain = animals[i]->getBrain();
-		if (brain)
-			std::cout << animals[i]->getType() << " " << i / 2 <<
-			" is thinking about : " << brain->getIdea(i % 5) << std::endl;
-	}
-}
-
 int main() {
 	//Animal	animal("test");
 	const Animal* j = new Dog();
