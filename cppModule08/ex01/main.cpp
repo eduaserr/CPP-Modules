6b032cb9ce69fd7bb84a4cc706cfe10b98de9f83
eduaserr <eduaserr@student.42malaga.com>
2026-07-11 04:17:24 +0200

    updates

--- cppModule08/ex01/main.cpp
+++ cppModule08/ex01/main.cpp
@@ -7,51 +7,51 @@
 
 /*int main()
 {
-    Span sp(100);
+	Span sp(100);
 
-    std::vector<int> v;
+	std::vector<int> v;
 
-    std::srand(std::time(NULL));
+	std::srand(std::time(NULL));
 
-    for (int i = 0; i < 100; i++)
-        v.push_back(std::rand());
+	for (int i = 0; i < 100; i++)
+		v.push_back(std::rand());
 
-    try
-    {
-        sp.addNumbers(v.begin(), v.end());
+	try
+	{
+		sp.addNumbers(v.begin(), v.end());
 
-        std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
-        std::cout << "Longest span : " << sp.longestSpan() << std::endl;
-    }
-    catch (std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
+		std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+		std::cout << "Longest span : " << sp.longestSpan() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
-    return 0;
+	return 0;
 }*/
 
 
 int main()
 {
-    Span sp(5);
+	Span sp(5);
 
-    std::vector<int> v;
+	std::vector<int> v;
 
-    sp.addNumber(INT_MIN);
-    sp.addNumber(INT_MAX);
+	sp.addNumber(INT_MIN);
+	sp.addNumber(INT_MAX);
 
-    try
-    {
-        sp.addNumbers(v.begin(), v.end());
+	try
+	{
+		sp.addNumbers(v.begin(), v.end());
 
-        std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
-        std::cout << "Longest span : " << sp.longestSpan() << std::endl;
-    }
-    catch (std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
+		std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+		std::cout << "Longest span : " << sp.longestSpan() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
-    return 0;
+	return 0;
 }
