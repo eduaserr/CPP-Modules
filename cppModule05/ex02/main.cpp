ce49b15726657c58f9c3812ab2466bbd627be192
eduaserr <157403329+eduaserr@users.noreply.github.com>
2026-07-13 19:36:54 +0200

    updates

--- cppModule05/ex02/main.cpp
+++ cppModule05/ex02/main.cpp
@@ -6,43 +6,43 @@
 #include <ctime>
 
 int main() {
-    srand(time(0));
+	srand(time(0));
 
-    std::cout << "=== Bureaucrat Creation ===\n";
-    Bureaucrat boss("Boss", 1);
-    Bureaucrat mid("Mid", 50);
-    Bureaucrat intern("Intern", 150);
-    std::cout << boss << "\n" << mid << "\n" << intern << "\n\n";
+	std::cout << "=== Bureaucrat Creation ===\n";
+	Bureaucrat boss("Boss", 1);
+	Bureaucrat mid("Mid", 50);
+	Bureaucrat intern("Intern", 150);
+	std::cout << boss << "\n" << mid << "\n" << intern << "\n\n";
 
-    std::cout << "=== Forms Creation ===\n";
-    ShrubberyCreationForm shrub("home");
-    RobotomyRequestForm robo("target");
-    PresidentialPardonForm pardon("prisoner");
-    std::cout << shrub << "\n" << robo << "\n" << pardon << "\n\n";
+	std::cout << "=== Forms Creation ===\n";
+	ShrubberyCreationForm shrub("home");
+	RobotomyRequestForm robo("target");
+	PresidentialPardonForm pardon("prisoner");
+	std::cout << shrub << "\n" << robo << "\n" << pardon << "\n\n";
 
-    std::cout << "=== Shrubbery ===\n";
-    intern.signForm(shrub);		// fail: 150 > 145
-    boss.signForm(shrub);		// success: 1 <= 145
-    boss.signForm(shrub);		// fail: already signed
-    intern.executeForm(shrub);	// fail: 150 > 137
-    boss.executeForm(shrub);	// success: creates file
+	std::cout << "=== Shrubbery ===\n";
+	intern.signForm(shrub);		// fail: 150 > 145
+	boss.signForm(shrub);		// success: 1 <= 145
+	boss.signForm(shrub);		// fail: already signed
+	intern.executeForm(shrub);	// fail: 150 > 137
+	boss.executeForm(shrub);	// success: creates file
 
-    std::cout << "\n=== Robotomy ===\n";
-    intern.signForm(robo);		// fail: 150 > 72
-    mid.signForm(robo);			// success: 50 <= 72
-    boss.signForm(robo);		// fail: already signed
-    intern.executeForm(robo);	// fail: 150 > 45
-    mid.executeForm(robo);		// fail: 50 > 45
-    boss.executeForm(robo);		// success: 50/50
-    boss.executeForm(robo);		// success: 50/50 again
+	std::cout << "\n=== Robotomy ===\n";
+	intern.signForm(robo);		// fail: 150 > 72
+	mid.signForm(robo);			// success: 50 <= 72
+	boss.signForm(robo);		// fail: already signed
+	intern.executeForm(robo);	// fail: 150 > 45
+	mid.executeForm(robo);		// fail: 50 > 45
+	boss.executeForm(robo);		// success: 50/50
+	boss.executeForm(robo);		// success: 50/50 again
 
-    std::cout << "\n=== Presidential ===\n";
-    intern.signForm(pardon);	// fail: 150 > 25
-    mid.signForm(pardon);		// fail: 50 > 25
-    boss.signForm(pardon);		// success: 1 <= 25
-    intern.executeForm(pardon);	// fail: 150 > 5
-    mid.executeForm(pardon);	// fail: 50 > 5
-    boss.executeForm(pardon);	// success
+	std::cout << "\n=== Presidential ===\n";
+	intern.signForm(pardon);	// fail: 150 > 25
+	mid.signForm(pardon);		// fail: 50 > 25
+	boss.signForm(pardon);		// success: 1 <= 25
+	intern.executeForm(pardon);	// fail: 150 > 5
+	mid.executeForm(pardon);	// fail: 50 > 5
+	boss.executeForm(pardon);	// success
 
-    return 0;
+	return 0;
 }
--- cppModule05/ex03/main.cpp
+++ cppModule05/ex03/main.cpp
@@ -5,29 +5,29 @@
 #include <iostream>
 
 int main() {
-    std::srand(std::time(0));
+	std::srand(std::time(0));
 
-    Intern intern;
-    Bureaucrat boss("Boss", 1);
+	Intern intern;
+	Bureaucrat boss("Boss", 1);
 
-    AForm* f;
+	AForm* f;
 
-    // robotomy
-    f = intern.makeForm("robotomy request", "Bender");
-    if (f) { boss.signForm(*f); boss.executeForm(*f); delete f; }
+	// robotomy
+	f = intern.makeForm("robotomy request", "Bender");
+	if (f) { boss.signForm(*f); boss.executeForm(*f); delete f; }
 
-    // ahruberry
-    f = intern.makeForm("shrubbery creation", "home");
-    if (f) { boss.signForm(*f); boss.executeForm(*f); delete f; }
+	// ahruberry
+	f = intern.makeForm("shrubbery creation", "home");
+	if (f) { boss.signForm(*f); boss.executeForm(*f); delete f; }
 
-    // presidential
-    f = intern.makeForm("presidential pardon", "Arthur Dent");
-    if (f) { boss.signForm(*f); boss.executeForm(*f); delete f; }
+	// presidential
+	f = intern.makeForm("presidential pardon", "Arthur Dent");
+	if (f) { boss.signForm(*f); boss.executeForm(*f); delete f; }
 
-    // invalid name
-    f = intern.makeForm("coffee request", "office");
-    if (!f) std::cout << "Invalid form test OK\n";
-    else delete f;
+	// invalid name
+	f = intern.makeForm("coffee request", "office");
+	if (!f) std::cout << "Invalid form test OK\n";
+	else delete f;
 
-    return 0;
+	return 0;
 }
