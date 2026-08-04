1f87129afbac2dd51ba56a3c9afb6d24bcd7bdc1
eduaserr <157403329+eduaserr@users.noreply.github.com>
2026-08-04 17:50:25 +0200

    ex01 & 3x02 updates

--- cppModule08/ex01/main.cpp
+++ cppModule08/ex01/main.cpp
@@ -5,7 +5,7 @@
 #include <ctime>
 #include <climits>
 
-/*int main()
+int main()
 {
 	Span sp(100);
 
@@ -29,29 +29,29 @@
 	}
 
 	return 0;
-}*/
-
-
-int main()
-{
-	Span sp(5);
-
-	std::vector<int> v;
-
-	sp.addNumber(INT_MIN);
-	sp.addNumber(INT_MAX);
-
-	try
-	{
-		sp.addNumbers(v.begin(), v.end());
-
-		std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
-		std::cout << "Longest span : " << sp.longestSpan() << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-
-	return 0;
 }
+
+
+// int main()
+// {
+// 	Span sp(5);
+
+// 	std::vector<int> v;
+
+// 	sp.addNumber(INT_MIN);
+// 	sp.addNumber(INT_MAX);
+
+// 	try
+// 	{
+// 		sp.addNumbers(v.begin(), v.end());
+
+// 		std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+// 		std::cout << "Longest span : " << sp.longestSpan() << std::endl;
+// 	}
+// 	catch (std::exception &e)
+// 	{
+// 		std::cout << e.what() << std::endl;
+// 	}
+
+// 	return 0;
+// }
--- cppModule08/ex02/main.cpp
+++ cppModule08/ex02/main.cpp
@@ -8,11 +8,15 @@ int main()
 	mstack.push(5);
 	mstack.push(17);
 
+	std::cout << "Size: " << mstack.size() << std::endl;
 	std::cout << "Top: " << mstack.top() << std::endl;
 
 	mstack.pop();
 
 	std::cout << "Size: " << mstack.size() << std::endl;
+	std::cout << "Top: " << mstack.top() << std::endl;
+
+	mstack.pop();
 
 	mstack.push(3);
 	mstack.push(5);
@@ -22,9 +26,8 @@ int main()
 	std::cout << "\nMutantStack:" << std::endl;
 
 	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
 
-	while (it != ite)
+	while (it != mstack.end())
 	{
 		std::cout << *it << std::endl;
 		++it;
